47e23a32a1eee7bcf0d429c397f9e722e5afe379
Jonathan <Jonahtang738@gmail.com>
2022-06-30 23:25:15 -0700

    create operate function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,20 @@ function multiply(num1, num2){
 function divide (num1, num2){
     return num1/num2;
 }
+
+function operate (operator, num1, num2){
+    switch(operator){
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            substract(num1, num2);
+            break;
+        case "*":
+            multiple(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+    }
+}
