cdc7b38bab2ce2e2560f1e3c2e665fbf0180d4d6
Jonathan <Jonahtang738@gmail.com>
2022-07-01 19:26:53 -0700

    Create Result object to store displayed result value

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+let result = new Result();
+
+let numbers = document.querySelectorAll('button.num');
+numbers.forEach(function(number){
+    number.onclick = function(){
+        console.log(number.value);
+        result.addInput(number.value);
+        console.log(result.value);
+    };
+});
+
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -29,4 +41,22 @@ function operate (operator, num1, num2){
             divide(num1, num2);
             break;
     }
+}
+
+let operator = document.querySelector('button.operator');
+console.log("test " + operator.value);
+
+function Result(){
+    this.value = '';
+    let operator = null;
+
+    this.changeOperator = function (newOperator){
+        operator = newOperator;
+    };
+
+    this.addInput = function(numStr){
+        this.value += numStr;
+    };
+    
+
 }
